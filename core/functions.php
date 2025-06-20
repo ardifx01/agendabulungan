1659d81d4fc11b8608734c41a37c91e989fd9bc9
Seggaf <seggaf@gmail.com>
2025-06-20 11:06:49 +0800

    fix nama bug, double function is admin

--- core/functions.php
+++ core/functions.php
@@ -4,9 +4,3 @@ if (!function_exists('e')) {
         return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
     }
 }
-
-if (!function_exists('is_admin')) {
-    function is_admin() {
-        return isset($_SESSION['user']) && $_SESSION['user']['role'] === 'admin';
-    }
-}
